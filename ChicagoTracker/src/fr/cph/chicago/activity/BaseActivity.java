63bbecf61afc242387b06c4dd30e56660263a87f
carlphilipp <cp.harmant@gmail.com>
2014-02-21 23:13:23 -0600

    Updated Javadoc

--- ChicagoTracker/src/fr/cph/chicago/activity/BaseActivity.java
+++ ChicagoTracker/src/fr/cph/chicago/activity/BaseActivity.java
@@ -50,10 +50,11 @@ import fr.cph.chicago.task.CtaConnectTask;
 import fr.cph.chicago.util.Util;
 
 /**
- * This class represents the base activity of the app
+ * This class represents the base activity of the application It will load the loading screen and/or
+ * the main activity
  * 
  * @author Carl-Philipp Harmant
- * 
+ * @version 1
  */
 public class BaseActivity extends Activity {
 
@@ -77,10 +78,10 @@ public class BaseActivity extends Activity {
 		}
 
 		if (error) {
-			showProgress(true, null);
+			showProgress(true);
 			new LoadData().execute();
 		} else if (DataHolder.getInstance().getBusData() == null || DataHolder.getInstance().getTrainData() == null) {
-			showProgress(true, null);
+			showProgress(true);
 			new LoadData().execute();
 		} else {
 			startMainActivity();
@@ -89,7 +90,25 @@ public class BaseActivity extends Activity {
 	}
 
 	/**
-	 * Load Bus and train data into DataHolder
+	 * Called via reflection from CtaConnectTask. It load arrivals data into ChicagoTracker object.
+	 * Update last update time. Start main activity
+	 * 
+	 * @param trainArrivals
+	 *            list of train arrivals
+	 * @param busArrivals
+	 *            list of bus arrivals
+	 */
+	public final void reloadData(final SparseArray<TrainArrival> trainArrivals, final List<BusArrival> busArrivals) {
+		ChicagoTracker.setBusArrivals(busArrivals);
+		ChicagoTracker.setTrainArrivals(trainArrivals);
+		ChicagoTracker.modifyLastUpdate(Calendar.getInstance().getTime());
+		startMainActivity();
+	}
+
+	/**
+	 * Load Bus and train data into DataHolder. The data are load in a sequence mode. It means that
+	 * if one of the url contacted does not response, we will still process the other data, and
+	 * won't throw any exception
 	 * 
 	 * @author Carl-Philipp Harmant
 	 * 
@@ -113,8 +132,6 @@ public class BaseActivity extends Activity {
 			this.busData = BusData.getInstance();
 			this.busData.readBusStops();
 
-			this.alertData = AlertData.getInstance();
-
 			try {
 				this.busData.loadBusRoutes();
 			} catch (ParserException e) {
@@ -123,6 +140,8 @@ public class BaseActivity extends Activity {
 				Log.e(TAG, e.getMessage(), e);
 			}
 
+			// Load alert API data
+			this.alertData = AlertData.getInstance();
 			try {
 				this.alertData.loadGeneralAlerts();
 			} catch (ParserException e) {
@@ -135,11 +154,13 @@ public class BaseActivity extends Activity {
 
 		@Override
 		protected final void onPostExecute(final Void result) {
+			// Put data into data holder
 			DataHolder dataHolder = DataHolder.getInstance();
 			dataHolder.setBusData(busData);
 			dataHolder.setTrainData(trainData);
 			dataHolder.setAlertData(alertData);
 			try {
+				// Load favorites data
 				loadData();
 			} catch (ParserException e) {
 				displayError(e);
@@ -152,11 +173,9 @@ public class BaseActivity extends Activity {
 	 * 
 	 * @param show
 	 *            show the bar or not
-	 * @param errorMessage
-	 *            the error message
 	 */
 	@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-	private final void showProgress(final boolean show, final String errorMessage) {
+	private final void showProgress(final boolean show) {
 		try {
 			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
 				int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
@@ -176,18 +195,27 @@ public class BaseActivity extends Activity {
 	}
 
 	/**
-	 * Load error
+	 * Display error. Set train and bus data to null before running the error activity
 	 * 
+	 * @param exceptionToBeThrown
+	 *            the exception that has been thrown
 	 */
-	public final void displayError(final TrackerException exceptionToBeThrown) {
+	private void displayError(final TrackerException exceptionToBeThrown) {
 		DataHolder.getInstance().setTrainData(null);
 		DataHolder.getInstance().setBusData(null);
-		showProgress(false, null);
+		// Stop of the laoding
+		showProgress(false);
 		ChicagoTracker.displayError(this, exceptionToBeThrown);
 		overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
 	}
 
-	public final void loadData() throws ParserException {
+	/**
+	 * Connect to CTA API and get arrivals trains and buses from favorites
+	 * 
+	 * @throws ParserException
+	 *             the exception
+	 */
+	private void loadData() throws ParserException {
 		MultiMap<String, String> params = new MultiValueMap<String, String>();
 		List<Integer> favorites = Preferences.getTrainFavorites(ChicagoTracker.PREFERENCE_FAVORITES_TRAIN);
 		for (Integer fav : favorites) {
@@ -207,18 +235,14 @@ public class BaseActivity extends Activity {
 		task.execute((Void) null);
 	}
 
-	public final void startMainActivity() {
+	/**
+	 * Finish current activity and start main activity with custom transition
+	 */
+	private void startMainActivity() {
 		Intent intent = new Intent(this, MainActivity.class);
-		showProgress(false, null);
+		showProgress(false);
 		finish();
 		startActivity(intent);
 		overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-	}
-
-	public final void reloadData(final SparseArray<TrainArrival> trainArrivals, final List<BusArrival> busArrivals) {
-		ChicagoTracker.setBusArrivals(busArrivals);
-		ChicagoTracker.setTrainArrivals(trainArrivals);
-		ChicagoTracker.modifyLastUpdate(Calendar.getInstance().getTime());
-		startMainActivity();
 	}
 }
