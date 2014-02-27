21db21ab5c2d53c2fc81e1c3a109602bc46bb13e
carlphilipp <cp.harmant@gmail.com>
2014-02-26 23:25:33 -0600

    Added n/a train line due to API weird data

--- ChicagoTracker/src/fr/cph/chicago/entity/enumeration/TrainLine.java
+++ ChicagoTracker/src/fr/cph/chicago/entity/enumeration/TrainLine.java
@@ -25,11 +25,16 @@ import android.graphics.Color;
  * @version 1
  */
 public enum TrainLine {
-	BLUE("Blue", "Blue", "Blue line", Color.rgb(0, 0, 204)), BROWN("Brn", "Brown", "Brown line", Color.rgb(102, 51, 0)), GREEN("G", "Green",
-			"Green line", Color.rgb(0, 153, 0)), ORANGE("Org", "Orange", "Orange line", Color.rgb(255, 128, 0)), PINK("Pink", "Pink", "Pink line",
-			Color.rgb(204, 0, 102)), PURPLE("P", "Purple", "Purple line", Color.rgb(102, 0, 102)),
+	BLUE("Blue", "Blue", "Blue line", Color.rgb(0, 0, 204)), 
+	BROWN("Brn", "Brown", "Brown line", Color.rgb(102, 51, 0)), 
+	GREEN("G", "Green","Green line", Color.rgb(0, 153, 0)), 
+	ORANGE("Org", "Orange", "Orange line", Color.rgb(255, 128, 0)), 
+	PINK("Pink", "Pink", "Pink line",Color.rgb(204, 0, 102)), 
+	PURPLE("P", "Purple", "Purple line", Color.rgb(102, 0, 102)),
 	// PURPLE_EXPRESS("Pexp", "Purple Express","Purple line express", Color.rgb(102, 0, 102)),
-	RED("Red", "Red", "Red line", Color.rgb(240, 0, 0)), YELLOW("Y", "Yellow", "Yellow line", Color.rgb(255, 255, 0));
+	RED("Red", "Red", "Red line", Color.rgb(240, 0, 0)), 
+	YELLOW("Y", "Yellow", "Yellow line", Color.rgb(255, 255, 0)),
+	NA("N/A", "N/A", "N/A line", Color.BLACK);
 
 	/** The text **/
 	private String text;
