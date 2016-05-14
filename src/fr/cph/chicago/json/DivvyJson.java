07d55621fb7072282455529b6024527d83db3c00
Carl-Philipp Harmant <cp.harmant@gmail.com>
2016-05-14 11:32:10 -0500

    Fix proguard prop file with new jackson lib

--- src/fr/cph/chicago/json/DivvyJson.java
+++ src/fr/cph/chicago/json/DivvyJson.java
@@ -7,7 +7,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 import fr.cph.chicago.entity.BikeStation;
+import lombok.Data;
 
+@Data
 public class DivvyJson {
 
     @JsonProperty("executionTime")
@@ -16,10 +18,5 @@ public class DivvyJson {
     private List<BikeStation> stations;
 
     public DivvyJson() {
-    }
-
-    @NonNull
-    public List<BikeStation> getStations() {
-        return stations;
     }
 }
