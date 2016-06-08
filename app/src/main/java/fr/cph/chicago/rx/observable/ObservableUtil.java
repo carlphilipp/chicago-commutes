5110068423e942f9dbb9127ba98de7a6ff38ce48
Carl-Philipp Harmant <cp.harmant@gmail.com>
2016-06-07 22:26:47 -0500

    Rework on global directory/package organization

---
+++ app/src/main/java/fr/cph/chicago/rx/observable/ObservableUtil.java
@@ -0,0 +1,209 @@
+package fr.cph.chicago.rx.observable;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.util.SparseArray;
+
+import com.annimon.stream.Optional;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import fr.cph.chicago.app.App;
+import fr.cph.chicago.entity.BikeStation;
+import fr.cph.chicago.entity.Bus;
+import fr.cph.chicago.entity.BusArrival;
+import fr.cph.chicago.entity.BusDirections;
+import fr.cph.chicago.entity.BusPattern;
+import fr.cph.chicago.entity.BusRoute;
+import fr.cph.chicago.entity.BusStop;
+import fr.cph.chicago.entity.TrainArrival;
+import fr.cph.chicago.entity.dto.FavoritesDTO;
+import fr.cph.chicago.entity.dto.FirstLoadDTO;
+import fr.cph.chicago.service.BikeService;
+import fr.cph.chicago.service.BusService;
+import fr.cph.chicago.service.TrainService;
+import fr.cph.chicago.service.impl.BikeServiceImpl;
+import fr.cph.chicago.service.impl.BusServiceImpl;
+import fr.cph.chicago.service.impl.TrainServiceImpl;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
+public class ObservableUtil {
+
+    private static final String TAG = ObservableUtil.class.getSimpleName();
+
+    private static final TrainService TRAIN_SERVICE = new TrainServiceImpl();
+    private static final BusService BUS_SERVICE = new BusServiceImpl();
+    private static final BikeService BIKE_SERVICE = new BikeServiceImpl();
+
+    private ObservableUtil() {
+    }
+
+    public static Observable<SparseArray<TrainArrival>> createTrainArrivals(@NonNull final Context context) {
+        return Observable.create(
+            (Subscriber<? super SparseArray<TrainArrival>> subscriber) -> {
+                subscriber.onNext(TRAIN_SERVICE.loadFavoritesTrain(context));
+                subscriber.onCompleted();
+            })
+            .onErrorReturn(throwable -> {
+                Log.e(TAG, throwable.getMessage(), throwable);
+                return null;
+            })
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    public static Observable<List<BusArrival>> createBusArrivals(@NonNull final Context context) {
+        return Observable.create(
+            (Subscriber<? super List<BusArrival>> subscriber) -> {
+                subscriber.onNext(BUS_SERVICE.loadFavoritesBuses(context));
+                subscriber.onCompleted();
+            })
+            .onErrorReturn(throwable -> {
+                Log.e(TAG, throwable.getMessage(), throwable);
+                return null;
+            })
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    public static Observable<List<BikeStation>> createAllBikeStationsObservable(@NonNull final Context context) {
+        return Observable.create(
+            (Subscriber<? super List<BikeStation>> subscriber) -> {
+                subscriber.onNext(BIKE_SERVICE.loadAllBikes(context));
+                subscriber.onCompleted();
+            })
+            .onErrorReturn(throwable -> {
+                Log.e(TAG, throwable.getMessage(), throwable);
+                return null;
+            })
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    public static Observable<FavoritesDTO> createAllDataObservable(@NonNull final Context context) {
+        // Train online favorites
+        final Observable<SparseArray<TrainArrival>> trainArrivalsObservable = ObservableUtil.createTrainArrivals(context);
+        // Bus online favorites
+        final Observable<List<BusArrival>> busArrivalsObservable = ObservableUtil.createBusArrivals(context);
+        // Bikes online all stations
+        final Observable<List<BikeStation>> bikeStationsObservable = ObservableUtil.createAllBikeStationsObservable(context);
+        return Observable.zip(trainArrivalsObservable, busArrivalsObservable, bikeStationsObservable,
+            (trainArrivals, busArrivals, bikeStations) -> {
+                App.modifyLastUpdate(Calendar.getInstance().getTime());
+                final FavoritesDTO favoritesDTO = new FavoritesDTO();
+                favoritesDTO.setTrainArrivals(trainArrivals);
+                favoritesDTO.setBusArrivals(busArrivals);
+                favoritesDTO.setBikeStations(bikeStations);
+                if (trainArrivals == null) {
+                    favoritesDTO.setTrainError(true);
+                    favoritesDTO.setTrainArrivals(new SparseArray<>());
+                }
+                if (busArrivals == null) {
+                    favoritesDTO.setBusError(true);
+                    favoritesDTO.setBusArrivals(new ArrayList<>());
+                }
+                if (bikeStations == null) {
+                    favoritesDTO.setBikeError(true);
+                    favoritesDTO.setBikeStations(new ArrayList<>());
+                }
+                return favoritesDTO;
+            });
+    }
+
+    public static Observable<List<BusStop>> createBusStopBoundObservable(@NonNull final Context context, @NonNull final String stopId, @NonNull final String bound) {
+        return Observable.create(
+            (Subscriber<? super List<BusStop>> subscriber) -> {
+                subscriber.onNext(BUS_SERVICE.loadOneBusStop(context, stopId, bound));
+                subscriber.onCompleted();
+            })
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    public static Observable<BusDirections> createBusDirectionsObservable(@NonNull final Context context, @NonNull final String busRouteId) {
+        return Observable.create(
+            (Subscriber<? super BusDirections> subscriber) -> {
+                subscriber.onNext(BUS_SERVICE.loadBusDirections(context, busRouteId));
+                subscriber.onCompleted();
+            })
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    public static Observable<FirstLoadDTO> createOnFirstLoadObservable(@NonNull final Context context) {
+        final Observable<List<BusRoute>> busRoutesObs = Observable.create(
+            (Subscriber<? super List<BusRoute>> subscriber) -> {
+                subscriber.onNext(BUS_SERVICE.loadBusRoutes(context));
+                subscriber.onCompleted();
+            })
+            .onErrorReturn(throwable -> {
+                Log.e(TAG, throwable.getMessage(), throwable);
+                return null;
+            })
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread());
+
+        final Observable<List<BikeStation>> bikeStationsObs = Observable.create(
+            (Subscriber<? super List<BikeStation>> subscriber) -> {
+                subscriber.onNext(BIKE_SERVICE.loadAllBikes(context));
+                subscriber.onCompleted();
+            })
+            .onErrorReturn(throwable -> {
+                Log.e(TAG, throwable.getMessage(), throwable);
+                return null;
+            })
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread());
+
+        return Observable.zip(busRoutesObs, bikeStationsObs, (busRoutes, bikeStations) -> {
+            final FirstLoadDTO result = new FirstLoadDTO();
+            if (busRoutes == null) {
+                busRoutes = new ArrayList<>();
+                result.setBusRoutesError(true);
+            }
+            if (bikeStations == null) {
+                bikeStations = new ArrayList<>();
+                result.setBikeStationsError(true);
+            }
+            result.setBusRoutes(busRoutes);
+            result.setBikeStations(bikeStations);
+            return result;
+        });
+    }
+
+    public static Observable<List<BusArrival>> createFollowBusObservable(@NonNull final Context context, @NonNull final String busId) {
+        return Observable.create(
+            (Subscriber<? super List<BusArrival>> subscriber) -> {
+                subscriber.onNext(BUS_SERVICE.loadFollowBus(context, busId));
+                subscriber.onCompleted();
+            })
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    public static Observable<Optional<BusPattern>> createBusPatternObservable(@NonNull final Context context, @NonNull final String busRouteId, @NonNull final String bound) {
+        return Observable.create(
+            (Subscriber<? super Optional<BusPattern>> subscriber) -> {
+                subscriber.onNext(BUS_SERVICE.loadBusPattern(context, busRouteId, bound));
+                subscriber.onCompleted();
+            })
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    public static Observable<List<Bus>> createBusListObservable(@NonNull final Context context, final int busId, @NonNull final String busRouteId) {
+        return Observable.create(
+            (Subscriber<? super List<Bus>> subscriber) -> {
+                subscriber.onNext(BUS_SERVICE.loadBus(context, busId, busRouteId));
+                subscriber.onCompleted();
+            })
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread());
+    }
+}
---
+++ app/src/main/java/fr/cph/chicago/service/impl/TrainServiceImpl.java
@@ -0,0 +1,132 @@
+package fr.cph.chicago.service.impl;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.util.SparseArray;
+
+import com.annimon.stream.Collectors;
+import com.annimon.stream.Optional;
+import com.annimon.stream.Stream;
+
+import org.apache.commons.collections4.MultiValuedMap;
+import org.apache.commons.collections4.multimap.ArrayListValuedHashMap;
+
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import fr.cph.chicago.R;
+import fr.cph.chicago.connection.CtaConnect;
+import fr.cph.chicago.data.DataHolder;
+import fr.cph.chicago.data.Preferences;
+import fr.cph.chicago.data.TrainData;
+import fr.cph.chicago.entity.Eta;
+import fr.cph.chicago.entity.TrainArrival;
+import fr.cph.chicago.entity.enumeration.TrainDirection;
+import fr.cph.chicago.entity.enumeration.TrainLine;
+import fr.cph.chicago.parser.XmlParser;
+import fr.cph.chicago.service.TrainService;
+import fr.cph.chicago.util.Util;
+import rx.exceptions.Exceptions;
+
+import static fr.cph.chicago.connection.CtaRequestType.TRAIN_ARRIVALS;
+
+public class TrainServiceImpl implements TrainService {
+
+    private final XmlParser xmlParser;
+
+    public TrainServiceImpl() {
+        this.xmlParser = XmlParser.getInstance();
+    }
+
+    @NonNull
+    @Override
+    public SparseArray<TrainArrival> loadFavoritesTrain(@NonNull final Context context) {
+        final MultiValuedMap<String, String> trainParams = Util.getFavoritesTrainParams(context);
+        Log.e("TrainSerImpl", "" + trainParams);
+        SparseArray<TrainArrival> trainArrivals = new SparseArray<>();
+        try {
+            final CtaConnect ctaConnect = CtaConnect.getInstance(context);
+            for (final Map.Entry<String, Collection<String>> entry : trainParams.asMap().entrySet()) {
+                final String key = entry.getKey();
+                if ("mapid".equals(key)) {
+                    final List<String> list = (List<String>) entry.getValue();
+                    if (list.size() < 5) {
+                        final InputStream xmlResult = ctaConnect.connect(context, TRAIN_ARRIVALS, trainParams);
+                        trainArrivals = xmlParser.parseArrivals(xmlResult, DataHolder.getInstance().getTrainData());
+                    } else {
+                        final int size = list.size();
+                        int start = 0;
+                        int end = 4;
+                        while (end < size + 1) {
+                            final List<String> subList = list.subList(start, end);
+                            final MultiValuedMap<String, String> paramsTemp = new ArrayListValuedHashMap<>();
+                            for (final String sub : subList) {
+                                paramsTemp.put(key, sub);
+                            }
+                            final InputStream xmlResult = ctaConnect.connect(context, TRAIN_ARRIVALS, paramsTemp);
+                            final SparseArray<TrainArrival> temp = xmlParser.parseArrivals(xmlResult, DataHolder.getInstance().getTrainData());
+                            for (int j = 0; j < temp.size(); j++) {
+                                trainArrivals.put(temp.keyAt(j), temp.valueAt(j));
+                            }
+                            start = end;
+                            if (end + 3 >= size - 1 && end != size) {
+                                end = size;
+                            } else {
+                                end = end + 3;
+                            }
+                        }
+                    }
+                }
+            }
+
+            // Apply filters
+            int index = 0;
+            while (index < trainArrivals.size()) {
+                final TrainArrival trainArrival = trainArrivals.valueAt(index++);
+                final List<Eta> etas = trainArrival.getEtas();
+                trainArrival.setEtas(Stream.of(etas)
+                    .filter(eta -> {
+                        final TrainLine line = eta.getRouteName();
+                        final TrainDirection direction = eta.getStop().getDirection();
+                        return Preferences.getTrainFilter(context, eta.getStation().getId(), line, direction);
+                    })
+                    .sorted()
+                    .collect(Collectors.toList())
+                );
+            }
+        } catch (final Throwable e) {
+            throw Exceptions.propagate(e);
+        }
+        return trainArrivals;
+    }
+
+    @NonNull
+    @Override
+    public TrainData loadLocalTrainData(@NonNull final Context context) {
+        TrainData.getInstance(context).read();
+        return TrainData.getInstance(context);
+    }
+
+    @NonNull
+    @Override
+    public Optional<TrainArrival> loadStationTrainArrival(@NonNull final Context context, int stationId) {
+        try {
+            final MultiValuedMap<String, String> params = new ArrayListValuedHashMap<>();
+            params.put(context.getString(R.string.request_map_id), Integer.toString(stationId));
+
+            final XmlParser xml = XmlParser.getInstance();
+            final InputStream xmlResult = CtaConnect.getInstance(context).connect(context, TRAIN_ARRIVALS, params);
+            final SparseArray<TrainArrival> arrivals = xml.parseArrivals(xmlResult, TrainData.getInstance(context));
+            if (arrivals.size() == 1) {
+                return Optional.of(arrivals.get(stationId));
+            } else {
+                return Optional.empty();
+            }
+        } catch (final Throwable e) {
+            throw Exceptions.propagate(e);
+        }
+    }
+}
