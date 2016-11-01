6826f45032ef0189c0e0d3a47c4106326b2ec26c
Carl-Philipp Harmant <cp.harmant@gmail.com>
2016-11-01 00:38:45 -0500

    Clean code

--- update-app/src/main/java/fr/cph/chicago/update/UpdateStops.java
+++ update-app/src/main/java/fr/cph/chicago/update/UpdateStops.java
@@ -9,42 +9,48 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Collections;
-import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 /**
+ * Download the last google_transit archive for Chicago and update stops.txt used in
+ * the android app.
+ *
  * @author cpharmant
  */
 public class UpdateStops {
+
     public static void main(String[] args) {
-        System.out.println("hello world!");
-        new UpdateStops().run();
+        Stream.of(args).forEach(System.out::println);
+        new UpdateStops(args[0], args[1]).run();
     }
 
     private static String STOP_FILE_NAME = "stops.txt";
-    private static String WORK_DIRECTORY = "src/test/";
 
-    public void run() {
-        // Download archive
-        Optional<File> fileOptional = downloadFile();
+    private String tempDirectory;
+    private String destinationDirectory;
+
+    private UpdateStops(final String tempDirectory, final String destinationDirectory) {
+        this.tempDirectory = tempDirectory;
+        this.destinationDirectory = destinationDirectory;
+    }
+
+    private void run() {
+        final Optional<File> fileOptional = downloadFile();
         fileOptional.ifPresent(downloadedFile ->
-            extractFile(downloadedFile).ifPresent(stopFile -> {
-                moveFile(stopFile);
-                cleanWorkingDirectory();
-            })
+            extractFile(downloadedFile).ifPresent(this::moveFile)
         );
     }
 
     private Optional<File> downloadFile() {
         try {
-            URL url = new URL("http://www.transitchicago.com/downloads/sch_data/google_transit.zip");
-            File file = new File(WORK_DIRECTORY + "google_transit.zip");
+            final URL url = new URL("http://www.transitchicago.com/downloads/sch_data/google_transit.zip");
+            final File file = new File(tempDirectory + "google_transit.zip");
             System.out.println("Start downloading file...");
             FileUtils.copyInputStreamToFile(url.openStream(), file);
             System.out.println("Done downloading file! " + file.getAbsolutePath());
             return Optional.ofNullable(file);
-        } catch (IOException e) {
+        } catch (final IOException e) {
             e.printStackTrace();
             return Optional.empty();
         }
@@ -54,25 +60,24 @@ public class UpdateStops {
         ZipFile zf = null;
         try {
             zf = new ZipFile(file);
-            List<? extends ZipEntry> derp = Collections.list(zf.entries());
-            Optional<? extends ZipEntry> entry = Stream.of(derp)
+            final Optional<? extends ZipEntry> entry = Stream.of(Collections.list(zf.entries()))
                 .filter(value -> value.getName().equals(STOP_FILE_NAME))
                 .findFirst();
             if (entry.isPresent()) {
-                ZipEntry zipEntry = entry.get();
-                File stops = new File(WORK_DIRECTORY + STOP_FILE_NAME);
+                final ZipEntry zipEntry = entry.get();
+                final File stops = new File(tempDirectory + STOP_FILE_NAME);
                 FileUtils.copyInputStreamToFile(zf.getInputStream(zipEntry), stops);
                 return Optional.ofNullable(stops);
             } else {
                 return Optional.empty();
             }
-        } catch (IOException e) {
+        } catch (final IOException e) {
             e.printStackTrace();
         } finally {
             if (zf != null) {
                 try {
                     zf.close();
-                } catch (IOException e) {
+                } catch (IOException ignored) {
                 }
             }
         }
@@ -81,23 +86,10 @@ public class UpdateStops {
 
     private void moveFile(File file) {
         try {
-            System.out.print("Move file");
-            FileUtils.copyFile(file, new File("src/main/assets/stops.txt"));
+            System.out.println("Move file to " + destinationDirectory + STOP_FILE_NAME);
+            FileUtils.copyFile(file, new File(destinationDirectory + STOP_FILE_NAME));
         } catch (IOException e) {
             e.printStackTrace();
-        }
-    }
-
-    private void cleanWorkingDirectory() {
-        if (!new File(WORK_DIRECTORY + "google_transit.zip").delete()) {
-            System.out.println("Could not delete " + WORK_DIRECTORY + "google_transit.zip");
-        } else {
-            System.out.println("Delete " + WORK_DIRECTORY + "google_transit.zip");
-        }
-        if (!new File(WORK_DIRECTORY + STOP_FILE_NAME).delete()) {
-            System.out.println("Could not delete " + WORK_DIRECTORY + STOP_FILE_NAME);
-        } else {
-            System.out.println("Delete " + WORK_DIRECTORY + STOP_FILE_NAME);
         }
     }
 }
