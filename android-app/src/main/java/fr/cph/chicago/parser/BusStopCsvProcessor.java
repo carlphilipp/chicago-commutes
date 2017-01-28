59b84434f71e3eec0d436fd4d0c88641788f8a85
Carl-Philipp Harmant <cp.harmant@gmail.com>
2017-01-28 14:13:22 -0600

    Improve the way bus stops are parsed. Now do not add a bus that does not contain a stopCode

--- android-app/src/main/java/fr/cph/chicago/parser/BusStopCsvProcessor.java
+++ android-app/src/main/java/fr/cph/chicago/parser/BusStopCsvProcessor.java
@@ -38,23 +38,25 @@ class BusStopCsvProcessor implements RowProcessor {
     @Override
     public void rowProcessed(final String[] row, final ParsingContext context) {
         final int stopId = Integer.parseInt(row[0]); // stop_id
-        // String stopCode = TrainDirection.fromString(row[1]); // stop_code
-        final String stopName = row[2]; // stop_name
-        final String stopDesc = row[3]; // stop_desc
+        final int stopCode = row[1] == null ? -1 : Integer.parseInt(row[1]); // stop_code
+        if (stopCode != -1) {
+            final String stopName = row[2]; // stop_name
+            final String stopDesc = row[3]; // stop_desc
 
-        final double latitude = Double.parseDouble(row[4]);// stop_lat
-        final double longitude = Double.parseDouble(row[5]);// stop_lon
+            final double latitude = Double.parseDouble(row[4]);// stop_lat
+            final double longitude = Double.parseDouble(row[5]);// stop_lon
 
-        final BusStop busStop = new BusStop();
-        busStop.setId(stopId);
-        busStop.setName(stopName);
-        busStop.setDescription(stopDesc);
-        final Position position = new Position();
-        position.setLatitude(latitude);
-        position.setLongitude(longitude);
-        busStop.setPosition(position);
+            final BusStop busStop = new BusStop();
+            busStop.setId(stopId);
+            busStop.setName(stopName);
+            busStop.setDescription(stopDesc);
+            final Position position = new Position();
+            position.setLatitude(latitude);
+            position.setLongitude(longitude);
+            busStop.setPosition(position);
 
-        rows.add(busStop);
+            rows.add(busStop);
+        }
     }
 
     @Override
