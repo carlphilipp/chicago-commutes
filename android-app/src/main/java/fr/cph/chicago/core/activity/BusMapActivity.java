9adaa59d4fb9381203b09c97b228f7989d590052
Carl-Philipp Harmant <cp.harmant@gmail.com>
2017-08-18 15:12:55 -0500

    Move Kotlin files into its own source directory

--- android-app/src/main/java/fr/cph/chicago/core/activity/BusMapActivity.java
+++ android-app/src/main/java/fr/cph/chicago/core/activity/BusMapActivity.java
@@ -46,7 +46,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Objects;
 
 import butterknife.BindString;
 import butterknife.ButterKnife;
