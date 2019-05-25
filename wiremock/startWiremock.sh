9a7ab9f80535ef7fdef6f1b6b8bd14d25d22f69e
Carl-Philipp Harmant <cp.harmant@gmail.com>
2019-05-24 21:11:44 -0500

    Modify wiremock script

--- wiremock/startWiremock.sh
+++ wiremock/startWiremock.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
-java -jar wiremock-standalone-2.23.2.jar
+
+current_dir=`dirname $0`
+source $current_dir/VERSION
+
+jar_name="wiremock-standalone-$WIREMOCK_VERSION.jar"
+jar="$current_dir/$jar_name"
+
+if [ ! -f jar ]; then
+    echo "Downloading wiremock..."
+    wget -P $current_dir http://repo1.maven.org/maven2/com/github/tomakehurst/wiremock-standalone/$WIREMOCK_VERSION/$jar_name
+fi
+
+java -jar $jar
