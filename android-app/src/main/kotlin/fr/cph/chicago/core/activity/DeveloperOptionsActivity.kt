1bd71f63739e3ba1ae555621af5a75477ce30886
Carl-Philipp Harmant <cp.harmant@gmail.com>
2019-12-08 21:17:07 -0600

    Remove butterknife from developer options activity

--- android-app/src/main/kotlin/fr/cph/chicago/core/activity/DeveloperOptionsActivity.kt
+++ android-app/src/main/kotlin/fr/cph/chicago/core/activity/DeveloperOptionsActivity.kt
@@ -25,49 +25,52 @@ import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.annotation.LayoutRes
-import androidx.appcompat.widget.Toolbar
-import butterknife.BindView
+import androidx.appcompat.app.AppCompatActivity
 import fr.cph.chicago.R
-import fr.cph.chicago.core.activity.butterknife.ButterKnifeActivity
+import fr.cph.chicago.service.PreferenceService
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_developer_options.cacheDetailsLayout
+import kotlinx.android.synthetic.main.activity_developer_options.showCacheLayout
+import kotlinx.android.synthetic.main.toolbar.toolbar
 import timber.log.Timber
 
-class DeveloperOptionsActivity : ButterKnifeActivity(R.layout.activity_developer_options) {
+class DeveloperOptionsActivity : AppCompatActivity() {
 
-    @BindView(R.id.toolbar)
-    lateinit var toolbar: Toolbar
-    @BindView(R.id.show_cache)
-    lateinit var showCacheLayout: LinearLayout
-    @BindView(R.id.cache_details)
-    lateinit var cacheDetailsLayout: LinearLayout
+    companion object {
+        private val preferenceService = PreferenceService
+    }
 
-    override fun create(savedInstanceState: Bundle?) {
-        preferenceService.getAllFavorites()
-            .observeOn(Schedulers.computation())
-            .map { favorites -> favorites.preferences }
-            .flatMapObservable { preferences -> Observable.fromIterable(preferences) }
-            .filter { preference -> preference.favorites.isNotEmpty() }
-            .sorted { preference1, preference2 -> preference1.name.value.compareTo(preference2.name.value) }
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(
-                { preference ->
-                    addCacheData(preference.name.value, R.layout.cache_developer_options_title)
-                    preference.favorites.forEach { entry -> addCacheData(entry, R.layout.cache_developer_options_value) }
-                },
-                { throwable ->
-                    Timber.e(throwable)
-                })
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (!this.isFinishing) {
+            setContentView(R.layout.activity_developer_options)
+            preferenceService.getAllFavorites()
+                .observeOn(Schedulers.computation())
+                .map { favorites -> favorites.preferences }
+                .flatMapObservable { preferences -> Observable.fromIterable(preferences) }
+                .filter { preference -> preference.favorites.isNotEmpty() }
+                .sorted { preference1, preference2 -> preference1.name.value.compareTo(preference2.name.value) }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    { preference ->
+                        addCacheData(preference.name.value, R.layout.cache_developer_options_title)
+                        preference.favorites.forEach { entry -> addCacheData(entry, R.layout.cache_developer_options_value) }
+                    },
+                    { throwable ->
+                        Timber.e(throwable)
+                    })
 
-        showCacheLayout.setOnClickListener {
-            cacheDetailsLayout.visibility = if (cacheDetailsLayout.visibility == View.GONE) {
-                View.VISIBLE
-            } else {
-                View.GONE
+            showCacheLayout.setOnClickListener {
+                cacheDetailsLayout.visibility = if (cacheDetailsLayout.visibility == View.GONE) {
+                    View.VISIBLE
+                } else {
+                    View.GONE
+                }
             }
+            setToolBar()
         }
-        setToolBar()
     }
 
     private fun addCacheData(text: String, @LayoutRes layout: Int) {
