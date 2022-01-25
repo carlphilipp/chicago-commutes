e6230e182d250f8b1fcdc1e04bb79111f6e7fac9
Carl-Philipp Harmant <cp.harmant@gmail.com>
2022-01-25 12:10:05 -0600

    Update android tools build

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -8,7 +8,7 @@ object Android {
 
 object Version {
     const val kotlin =                  "1.+"
-    const val android_tools_build =     "7.0.3"
+    const val android_tools_build =     "7.+"
     const val realm =                   "10.+"
     const val gradle_version =          "0.36.0"
 
