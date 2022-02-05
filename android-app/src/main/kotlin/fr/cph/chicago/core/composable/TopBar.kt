b44b791dd6620226477327d2bc7546de452fbe9f
Carl-Philipp Harmant <cp.harmant@gmail.com>
2022-02-04 22:04:10 -0600

    format

--- android-app/src/main/kotlin/fr/cph/chicago/core/composable/TopBar.kt
+++ android-app/src/main/kotlin/fr/cph/chicago/core/composable/TopBar.kt
@@ -8,13 +8,14 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun TopBar(title: String, openDrawer: () -> Unit) {
     CenterAlignedTopAppBar(
-        title = { Text(title) },
+        title = { Text(text = title) },
         navigationIcon = {
             IconButton(onClick = { openDrawer() }) {
                 Icon(
@@ -33,6 +34,6 @@ fun TopBar(title: String, openDrawer: () -> Unit) {
                     contentDescription = "Search"
                 )
             }
-        }
+        },
     )
 }
