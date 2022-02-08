45635b3ab7c3e36494b5ddf15d2886a2f24d8f7f
Carl-Philipp Harmant <cp.harmant@gmail.com>
2022-02-08 17:51:29 -0600

    Try some animation

--- android-app/src/main/kotlin/fr/cph/chicago/core/composable/screen/Favorites.kt
+++ android-app/src/main/kotlin/fr/cph/chicago/core/composable/screen/Favorites.kt
@@ -1,5 +1,14 @@
 package fr.cph.chicago.core.composable.screen
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.SizeTransform
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.with
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -7,15 +16,20 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.Card
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.TextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Details
 import androidx.compose.material.icons.filled.DirectionsBike
@@ -25,15 +39,21 @@ import androidx.compose.material.icons.filled.Train
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
@@ -49,8 +69,8 @@ import fr.cph.chicago.core.model.enumeration.BusDirection
 import fr.cph.chicago.redux.FavoritesAction
 import fr.cph.chicago.redux.store
 import fr.cph.chicago.util.Util
-import timber.log.Timber
 import java.math.BigInteger
+import timber.log.Timber
 
 private val util = Util
 
@@ -135,6 +155,7 @@ fun Favorites() {
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class, androidx.compose.animation.ExperimentalAnimationApi::class)
 @Composable
 fun NewDesign(trainStation: TrainStation) {
     Column(modifier = Modifier.padding(start = 10.dp, end = 10.dp, top = 10.dp, bottom = 10.dp)) {
@@ -156,49 +177,208 @@ fun NewDesign(trainStation: TrainStation) {
         trainStation.lines.forEach { trainLine ->
             val arrivals = Favorites.getTrainArrivalByLine(trainStation.id, trainLine)
             for (entry in arrivals.entries) {
+                var expandedArrivals by remember { mutableStateOf<String?>(null) }
                 val title = entry.key
                 val value = entry.value
-                Row(verticalAlignment = Alignment.CenterVertically) {
+                val expendedId = trainStation.id.toString() + title + trainLine.toTextString()
+                var nextTrainTime by remember { mutableStateOf(value[0]) }
+                Surface(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(40.dp),
+                    //color = Color.Transparent,
+                    color = Color.Green,
+                    shape = RoundedCornerShape(20.0.dp),
+                ) {
+                    TextButton(
+                        onClick = { expandedArrivals = if (expandedArrivals == expendedId) null else expendedId },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight(),
+                    ) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.fillMaxWidth()
+                            //.clip(RoundedCornerShape(20.dp))
+                        ) {
+                            Box(
+                                modifier = Modifier
+                                    .size(20.dp)
+                                    .clip(RoundedCornerShape(3.dp))
+                                    .background(Color(trainLine.color))
+                                //.padding(start = 5.dp),
+                            )
+                            Text(
+                                text = title,
+                                maxLines = 1,
+                                style = MaterialTheme.typography.titleMedium,
+                                modifier = Modifier
+                                    .weight(1f)
+                                //.padding(horizontal = 10.dp),
+                            )
+
+                            //ArrivalTime2()
+                            Button(onClick = { nextTrainTime = nextTrainTime + "1" }) {
+                                Text("Add")
+                            }
+                            ArrivalTime(time = nextTrainTime, onTimeChange = { nextTrainTime = it })
+                            Text(
+                                text = value[0],
+                                style = MaterialTheme.typography.bodyLarge,
+                                color = Color(0xFF4f76bf),
+                                maxLines = 1,
+                            )
+                        }
+                    }
+                }
+/*                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.clickable {
+                        expandedArrivals = if (expandedArrivals == expendedId) null else expendedId
+                    }
+                        .padding(top = 10.dp, bottom = 10.dp)
+                        //.clip(RoundedCornerShape(20.dp))
+                ) {
                     Box(
                         modifier = Modifier
-                            .size(35.dp)
+                            .size(20.dp)
                             .clip(RoundedCornerShape(3.dp))
                             .background(Color(trainLine.color))
+                            .padding(start = 5.dp),
                     )
                     Text(
                         text = title,
                         maxLines = 1,
-                        style = MaterialTheme.typography.titleSmall,
+                        style = MaterialTheme.typography.titleMedium,
                         modifier = Modifier
                             .weight(1f)
-                            //.padding(horizontal = 10.dp),
+                            .padding(horizontal = 10.dp),
+                        //.padding(horizontal = 10.dp),
                     )
-                    Column(modifier = Modifier.width(100.dp)) {
-                        Text(
-                            text = value[0],
-                            style = MaterialTheme.typography.bodyLarge,
-                            color = Color(0xFF4f76bf),
-                            maxLines = 1,
-                        )
-                        Row(modifier = Modifier.background(Color.Red)) {
-                            for (index in 1 until value.size) {
-                                Text(
-                                    text = value[index],
-                                    style = MaterialTheme.typography.bodySmall,
-                                )
-                            }
+
+                    Text(
+                        text = value[0],
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = Color(0xFF4f76bf),
+                        maxLines = 1,
+                    )
+                }*/
+                val visible = expandedArrivals == expendedId
+                AnimatedVisibility(visible = visible) {
+                    Spacer(modifier = Modifier.height(8.dp))
+                }
+                if (visible) {
+                    Row(modifier = Modifier.background(Color.Red)) {
+                        for (index in 1 until value.size) {
+                            Text(
+                                text = value[index],
+                                style = MaterialTheme.typography.bodySmall,
+                            )
                         }
                     }
                 }
+                AnimatedVisibility(visible = visible) {
+                    Spacer(modifier = Modifier.height(8.dp))
+                }
+
             }
         }
     }
 }
 
-@Preview(
-    name = "NewDesign",
-    showBackground = true
-)
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun ArrivalTime(time: String, onTimeChange: (String) -> Unit) {
+    Row {
+        var expanded by remember { mutableStateOf(false) }
+        //var count by remember { mutableStateOf(time) }
+        //var timeState by remember { mutableStateOf(time) }
+        Surface(
+            color = MaterialTheme.colorScheme.primary,
+            //onClick = { expanded = !expanded }
+        ) {
+            AnimatedContent(
+                targetState = time,
+                transitionSpec = {
+                    // Compare the incoming number with the previous number.
+                    if (targetState > initialState) {
+                        // If the target number is larger, it slides up and fades in
+                        // while the initial (smaller) number slides up and fades out.
+                        slideInVertically { height -> height } + fadeIn() with
+                            slideOutVertically { height -> -height } + fadeOut()
+                    } else {
+                        // If the target number is smaller, it slides down and fades in
+                        // while the initial number slides down and fades out.
+                        slideInVertically { height -> -height } + fadeIn() with
+                            slideOutVertically { height -> height } + fadeOut()
+                    }.using(
+                        // Disable clipping since the faded slide-in/out should
+                        // be displayed out of bounds.
+                        SizeTransform(clip = false)
+                    )
+                }
+            ) { target ->
+                Text(
+                    text = "$target",
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = Color(0xFF4f76bf),
+                    maxLines = 1,
+                )
+            }
+        }
+
+    }
+}
+
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun ArrivalTime2(/*time: String*/) {
+    Row {
+        var expanded by remember { mutableStateOf(false) }
+        var count by remember { mutableStateOf(0) }
+        Surface(
+            color = MaterialTheme.colorScheme.primary,
+            onClick = { expanded = !expanded }
+        ) {
+            AnimatedContent(
+                targetState = count,
+                transitionSpec = {
+                    // Compare the incoming number with the previous number.
+                    if (targetState > initialState) {
+                        // If the target number is larger, it slides up and fades in
+                        // while the initial (smaller) number slides up and fades out.
+                        slideInVertically { height -> height } + fadeIn() with
+                            slideOutVertically { height -> -height } + fadeOut()
+                    } else {
+                        // If the target number is smaller, it slides down and fades in
+                        // while the initial number slides down and fades out.
+                        slideInVertically { height -> -height } + fadeIn() with
+                            slideOutVertically { height -> height } + fadeOut()
+                    }.using(
+                        // Disable clipping since the faded slide-in/out should
+                        // be displayed out of bounds.
+                        SizeTransform(clip = false)
+                    )
+                }
+            ) { target ->
+                Text(
+                    text = "$target",
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = Color(0xFF4f76bf),
+                    maxLines = 1,
+                )
+            }
+        }
+        Button(onClick = { count++ }) {
+            Text("Add")
+        }
+    }
+}
+
+class ArrivalTimePreviewParameterProvider() : PreviewParameterProvider<String> {
+    override val values = sequenceOf("10 min")
+}
+
 @Composable
 fun NewDesignPreview(@PreviewParameter(TrainStationProvider::class) trainStation: TrainStation) {
     ChicagoCommutesTheme {
