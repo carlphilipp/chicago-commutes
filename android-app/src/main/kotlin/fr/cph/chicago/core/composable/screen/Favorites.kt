67efd6835132c0868f22e25cc8b01361af9693b0
Carl-Philipp Harmant <cp.harmant@gmail.com>
2022-02-09 13:10:45 -0600

    Clean a lot of the favorites code

--- android-app/src/main/kotlin/fr/cph/chicago/core/composable/screen/Favorites.kt
+++ android-app/src/main/kotlin/fr/cph/chicago/core/composable/screen/Favorites.kt
@@ -21,24 +21,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
 import androidx.compose.material.Card
-import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Details
 import androidx.compose.material.icons.filled.DirectionsBike
 import androidx.compose.material.icons.filled.DirectionsBus
-import androidx.compose.material.icons.filled.Map
-import androidx.compose.material.icons.filled.PedalBike
 import androidx.compose.material.icons.filled.Train
-import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -48,7 +41,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
@@ -103,17 +95,16 @@ fun Favorites() {
     }
 }
 
-@OptIn(ExperimentalMaterialApi::class, ExperimentalAnimationApi::class)
 @Composable
 fun TrainFavoriteCard(modifier: Modifier = Modifier, trainStation: TrainStation, lastUpdate: LastUpdate) {
     FavoriteCardWrapper(modifier = modifier) {
 
-        HeaderCard2(name = trainStation.name, lines = trainStation.lines, image = Icons.Filled.Train, lastUpdate = lastUpdate)
+        HeaderCard(name = trainStation.name, lines = trainStation.lines, image = Icons.Filled.Train, lastUpdate = lastUpdate)
 
         trainStation.lines.forEach { trainLine ->
             val arrivals = Favorites.getTrainArrivalByLine(trainStation.id, trainLine)
             for (entry in arrivals.entries) {
-                TrainDirectionArrivals(
+                TrainArrivals(
                     trainLine = trainLine,
                     destination = entry.key,
                     arrivals = entry.value,
@@ -121,14 +112,14 @@ fun TrainFavoriteCard(modifier: Modifier = Modifier, trainStation: TrainStation,
             }
         }
 
-        FooterCard2()
+        FooterCard()
     }
 }
 
 @Composable
 fun BusFavoriteCard(modifier: Modifier = Modifier, busRoute: BusRoute, lastUpdate: LastUpdate) {
     FavoriteCardWrapper(modifier = modifier) {
-        HeaderCard2(name = "${busRoute.id} ${busRoute.name}", image = Icons.Filled.DirectionsBus, lastUpdate = lastUpdate)
+        HeaderCard(name = "${busRoute.id} ${busRoute.name}", image = Icons.Filled.DirectionsBus, lastUpdate = lastUpdate)
 
         val busArrivalDTO = Favorites.getBusArrivalsMapped(busRoute.id)
         for ((stopName, boundMap) in busArrivalDTO.entries) {
@@ -138,26 +129,26 @@ fun BusFavoriteCard(modifier: Modifier = Modifier, busRoute: BusRoute, lastUpdat
                 val busDirection = BusDirection.fromString(key)
                 // TODO: Handle different size for busdirection
                 val stopNameDisplay = if (busDirection == BusDirection.UNKNOWN) stopNameTrimmed else "$stopNameTrimmed ${busDirection.shortLowerCase}"
-                TrainDirectionArrivals(
+                TrainArrivals(
                     destination = stopNameDisplay,
                     arrivals = value.map { busArrival -> busArrival.timeLeftDueDelay }
                 )
             }
         }
 
-        FooterCard2()
+        FooterCard()
     }
 }
 
 @Composable
 fun BikeFavoriteCard(modifier: Modifier = Modifier, bikeStation: BikeStation, lastUpdate: LastUpdate) {
     FavoriteCardWrapper(modifier = modifier) {
-        HeaderCard2(name = bikeStation.name, image = Icons.Filled.DirectionsBike, lastUpdate = lastUpdate)
+        HeaderCard(name = bikeStation.name, image = Icons.Filled.DirectionsBike, lastUpdate = lastUpdate)
 
-        TrainDirectionArrivals(destination = "Available bikes", arrivals = listOf(bikeStation.availableBikes.toString()))
-        TrainDirectionArrivals(destination = "Available docks", arrivals = listOf(bikeStation.availableDocks.toString()))
+        TrainArrivals(destination = "Available bikes", arrivals = listOf(bikeStation.availableBikes.toString()))
+        TrainArrivals(destination = "Available docks", arrivals = listOf(bikeStation.availableDocks.toString()))
 
-        FooterCard2()
+        FooterCard()
     }
 }
 
@@ -169,7 +160,7 @@ fun FavoriteCardWrapper(modifier: Modifier = Modifier, content: @Composable Colu
 }
 
 @Composable
-fun HeaderCard2(modifier: Modifier = Modifier, name: String, lines: Set<TrainLine> = mutableSetOf(), image: ImageVector, lastUpdate: LastUpdate) {
+fun HeaderCard(modifier: Modifier = Modifier, name: String, lines: Set<TrainLine> = mutableSetOf(), image: ImageVector, lastUpdate: LastUpdate) {
     Row(
         horizontalArrangement = Arrangement.Start,
         verticalAlignment = Alignment.CenterVertically,
@@ -200,9 +191,11 @@ fun HeaderCard2(modifier: Modifier = Modifier, name: String, lines: Set<TrainLin
                 AnimatedText(time = lastUpdate.value, style = MaterialTheme.typography.labelSmall)
             }
         }
-        Row(horizontalArrangement = Arrangement.End, modifier = Modifier
-            .fillMaxWidth()
-            .padding(end = 12.dp)) {
+        Row(
+            horizontalArrangement = Arrangement.End, modifier = Modifier
+                .fillMaxWidth()
+                .padding(end = 12.dp)
+        ) {
             lines.forEach { trainLine ->
                 Box(
                     modifier = Modifier
@@ -217,7 +210,7 @@ fun HeaderCard2(modifier: Modifier = Modifier, name: String, lines: Set<TrainLin
 }
 
 @Composable
-fun FooterCard2(modifier: Modifier = Modifier, detailsOnClick: () -> Unit = {}, mapOnClick: () -> Unit = {}) {
+fun FooterCard(modifier: Modifier = Modifier, detailsOnClick: () -> Unit = {}, mapOnClick: () -> Unit = {}) {
     Row(
         horizontalArrangement = Arrangement.End,
         modifier = modifier
@@ -244,42 +237,36 @@ fun FooterCard2(modifier: Modifier = Modifier, detailsOnClick: () -> Unit = {}, 
 }
 
 @Composable
-fun TrainDirectionArrivals(modifier: Modifier = Modifier, trainLine: TrainLine = TrainLine.NA, destination: String, arrivals: List<String>) {
-    //var nextTrainTime by remember { mutableStateOf(arrivals[0]) }
-    TextButton(
-        onClick = { },
-        modifier = modifier
-    ) {
-        Column {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.fillMaxWidth(),
-            ) {
-                Box(
-                    modifier = Modifier
-                        .size(20.dp)
-                        .clip(RoundedCornerShape(3.dp))
-                        .background(Color(trainLine.color)),
+fun TrainArrivals(modifier: Modifier = Modifier, trainLine: TrainLine = TrainLine.NA, destination: String, arrivals: List<String>) {
+    Column(modifier = modifier.padding(start = 12.dp, end = 12.dp)) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 6.dp, bottom = 6.dp),
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(20.dp)
+                    .clip(RoundedCornerShape(3.dp))
+                    .background(Color(trainLine.color)),
+            )
+            Text(
+                text = destination,
+                maxLines = 1,
+                style = MaterialTheme.typography.titleSmall,
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(horizontal = 10.dp),
+            )
+            for (index in arrivals.indices) {
+                var nextTime by remember { mutableStateOf(arrivals[index]) }
+                nextTime = arrivals[index]
+                AnimatedText(
+                    time = nextTime,
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(start = 3.dp)
                 )
-                Text(
-                    text = destination,
-                    maxLines = 1,
-                    style = MaterialTheme.typography.titleSmall,
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(horizontal = 10.dp),
-                )
-                //nextTrainTime = arrivals[0]
-                //AnimatedText(time = nextTrainTime, style = MaterialTheme.typography.bodyMedium)
-                for (index in arrivals.indices) {
-                    var nextTime by remember { mutableStateOf(arrivals[index]) }
-                    nextTime = arrivals[index]
-                    AnimatedText(
-                        time = nextTime,
-                        style = MaterialTheme.typography.bodyMedium,
-                        modifier = Modifier.padding(start = 3.dp)
-                    )
-                }
             }
         }
     }
@@ -307,191 +294,5 @@ fun AnimatedText(modifier: Modifier = Modifier, time: String, style: TextStyle =
                 )
             }
         }
-    }
-}
-
-@OptIn(ExperimentalAnimationApi::class)
-@Composable
-fun ArrivalTime2(/*time: String*/) {
-    Row {
-        var expanded by remember { mutableStateOf(false) }
-        var count by remember { mutableStateOf(0) }
-        Surface(
-            color = MaterialTheme.colorScheme.primary,
-            onClick = { expanded = !expanded }
-        ) {
-            AnimatedContent(
-                targetState = count,
-                transitionSpec = {
-                    // Compare the incoming number with the previous number.
-                    if (targetState > initialState) {
-                        // If the target number is larger, it slides up and fades in
-                        // while the initial (smaller) number slides up and fades out.
-                        slideInVertically { height -> height } + fadeIn() with
-                            slideOutVertically { height -> -height } + fadeOut()
-                    } else {
-                        // If the target number is smaller, it slides down and fades in
-                        // while the initial number slides down and fades out.
-                        slideInVertically { height -> -height } + fadeIn() with
-                            slideOutVertically { height -> height } + fadeOut()
-                    }.using(
-                        // Disable clipping since the faded slide-in/out should
-                        // be displayed out of bounds.
-                        SizeTransform(clip = false)
-                    )
-                }
-            ) { target ->
-                Text(
-                    text = "$target",
-                    style = MaterialTheme.typography.bodyLarge,
-                    //color = Color(0xFF4f76bf),
-                    maxLines = 1,
-                )
-            }
-        }
-        Button(onClick = { count++ }) {
-            Text("Add")
-        }
-    }
-}
-
-@Composable
-fun HeaderCard(image: ImageVector, title: String, lastUpdate: String) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.padding(all = 0.dp)
-    ) {
-        Image(
-            imageVector = image,
-            contentDescription = "icon",
-            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.secondary),
-            modifier = Modifier
-                .size(55.dp)
-                .padding(10.dp),
-        )
-        Text(
-            text = title,
-            maxLines = 1,
-            //fontWeight = FontWeight.Bold,
-            //style = MaterialTheme.typography.bodyLarge,
-            modifier = Modifier.weight(1f),
-        )
-        Text(
-            text = lastUpdate,
-            maxLines = 1,
-            modifier = Modifier.padding(horizontal = 10.dp),
-        )
-    }
-}
-
-@Composable
-fun FooterCard() {
-    Row(
-        horizontalArrangement = Arrangement.End,
-        modifier = Modifier
-            .padding(all = 0.dp)
-            .fillMaxWidth()
-    ) {
-        ElevatedButton(
-            onClick = { store.dispatch(FavoritesAction()) },
-            modifier = Modifier.padding(3.dp),
-        ) {
-            Image(
-                imageVector = Icons.Filled.Details,
-                contentDescription = "Icon",
-                modifier = Modifier.padding(end = 5.dp),
-                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
-            )
-            Text(
-                text = "Details".uppercase(),
-            )
-        }
-        ElevatedButton(
-            onClick = {
-            },
-            modifier = Modifier.padding(3.dp),
-        ) {
-            Image(
-                imageVector = Icons.Filled.Map,
-                contentDescription = "Icon",
-                modifier = Modifier.padding(end = 5.dp),
-                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
-            )
-            Text(
-                text = "View Trains".uppercase(),
-            )
-        }
-    }
-}
-
-@Composable
-fun TrainArrivals(trainStation: TrainStation) {
-    ArrivalWrapper {
-        trainStation.lines.forEach { trainLine ->
-            val arrivals = Favorites.getTrainArrivalByLine(trainStation.id, trainLine)
-            for (entry in arrivals.entries) {
-                val text = entry.key
-                val arrival = entry.value.joinToString(" ")
-                ArrivalLine(boxColor = Color(trainLine.color), text, arrival)
-            }
-        }
-    }
-}
-
-@Composable
-fun BusArrivals(busRoute: BusRoute) {
-    ArrivalWrapper {
-        val busArrivalDTO = Favorites.getBusArrivalsMapped(busRoute.id)
-        for ((stopName, boundMap) in busArrivalDTO.entries) {
-            val stopNameTrimmed = util.trimBusStopNameIfNeeded(stopName)
-            for ((key, value) in boundMap) {
-                val (_, _, _, stopId, _, routeId, boundTitle) = value.iterator().next()
-                val busDirection = BusDirection.fromString(key)
-                // TODO: Handle different size for busdirection
-                val stopNameDisplay = if (busDirection == BusDirection.UNKNOWN) stopNameTrimmed else "$stopNameTrimmed ${busDirection.shortLowerCase}"
-                ArrivalLine(
-                    title = stopNameDisplay,
-                    value = value.joinToString(separator = " ") { busArrival -> busArrival.timeLeftDueDelay }
-                )
-            }
-        }
-    }
-}
-
-@Composable
-fun BikeData(bikeStation: BikeStation) {
-    ArrivalWrapper {
-        ArrivalLine(title = "Available bikes", value = bikeStation.availableBikes.toString())
-        ArrivalLine(title = "Available docks", value = bikeStation.availableDocks.toString())
-    }
-}
-
-@Composable
-fun ArrivalWrapper(content: @Composable ColumnScope.() -> Unit) {
-    Row {
-        Column(Modifier.padding(10.dp), content = content)
-    }
-}
-
-@Composable
-fun ArrivalLine(boxColor: Color = Color.Black, title: String, value: String) {
-    Row(verticalAlignment = Alignment.CenterVertically) {
-        Box(
-            modifier = Modifier
-                .size(15.dp)
-                .clip(RoundedCornerShape(3.dp))
-                .background(boxColor)
-        )
-        Text(
-            text = title,
-            maxLines = 1,
-            modifier = Modifier
-                .weight(1f)
-                .padding(horizontal = 10.dp),
-        )
-        Text(
-            text = value,
-            maxLines = 1,
-        )
     }
 }
