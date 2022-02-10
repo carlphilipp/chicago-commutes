ba3512cecf998a928d9b665bf64b2d6f415b1a2a
Carl-Philipp Harmant <cp.harmant@gmail.com>
2022-02-10 13:56:22 -0600

    Display arrival in train station composable

--- android-app/src/main/kotlin/fr/cph/chicago/core/composable/TrainStationComposable.kt
+++ android-app/src/main/kotlin/fr/cph/chicago/core/composable/TrainStationComposable.kt
@@ -14,6 +14,8 @@ import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.absoluteOffset
@@ -21,11 +23,14 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Map
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Icon
@@ -42,6 +47,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
@@ -56,20 +62,26 @@ import fr.cph.chicago.client.GoogleStreetClient
 import fr.cph.chicago.core.App
 import fr.cph.chicago.core.composable.theme.ChicagoCommutesTheme
 import fr.cph.chicago.core.model.Position
+import fr.cph.chicago.core.model.Stop
+import fr.cph.chicago.core.model.TrainEta
 import fr.cph.chicago.core.model.TrainStation
+import fr.cph.chicago.core.model.enumeration.TrainLine
 import fr.cph.chicago.redux.AddTrainFavoriteAction
 import fr.cph.chicago.redux.RemoveTrainFavoriteAction
 import fr.cph.chicago.redux.State
+import fr.cph.chicago.redux.Status
+import fr.cph.chicago.redux.TrainStationAction
 import fr.cph.chicago.redux.store
 import fr.cph.chicago.service.PreferenceService
 import fr.cph.chicago.service.TrainService
 import fr.cph.chicago.util.Util
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import java.math.BigInteger
-import java.util.Locale
 import kotlinx.coroutines.launch
 import org.rekotlin.StoreSubscriber
 import timber.log.Timber
+import java.math.BigInteger
+import java.util.Calendar
+import java.util.Locale
 
 private val googleStreetClient = GoogleStreetClient
 private val preferenceService = PreferenceService
@@ -80,18 +92,24 @@ private var isFavorite = mutableStateOf(false)
 class TrainStationComposable : ComponentActivity(), StoreSubscriber<State> {
     private var googleStreetMapImage = mutableStateOf<Drawable>(ShapeDrawable())
     private var showGoogleStreetImage = mutableStateOf(false)
+    private var trainEtasState = mutableStateOf(listOf<TrainEta>())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         googleStreetMapImage.value = AppCompatResources.getDrawable(App.instance.applicationContext, R.drawable.placeholder_street_view)!!
         val stationId = BigInteger(intent.extras?.getString(getString(R.string.bundle_train_stationId), "0")!!)
         isFavorite.value = isFavorite(stationId)
         val trainStation = TrainService.getStation(stationId)
+
         val position = trainStation.stops[0].position
         loadGoogleStreetImage(position)
+        store.subscribe(this)
+        store.dispatch(TrainStationAction(trainStation.id))
         setContent {
             ChicagoCommutesTheme {
                 TrainStationView(
                     trainStation = trainStation,
+                    trainEtas = trainEtasState.value,
                     googleStreetMapImage = googleStreetMapImage.value,
                     showGoogleStreetImage = showGoogleStreetImage.value,
                     isFavorite = isFavorite.value,
@@ -102,6 +120,21 @@ class TrainStationComposable : ComponentActivity(), StoreSubscriber<State> {
 
     override fun newState(state: State) {
         Timber.i("new state")
+        when (state.trainStationStatus) {
+            Status.SUCCESS -> {
+                trainEtasState.value = state.trainStationArrival.trainEtas
+            }
+            Status.FAILURE -> {
+                TODO()
+            }
+            Status.ADD_FAVORITES -> {
+                TODO()
+            }
+            Status.REMOVE_FAVORITES -> {
+                TODO()
+            }
+            else -> Timber.d("Status not handled")
+        }
     }
 
     @SuppressLint("CheckResult")
@@ -125,6 +158,7 @@ class TrainStationComposable : ComponentActivity(), StoreSubscriber<State> {
 fun TrainStationView(
     modifier: Modifier = Modifier,
     trainStation: TrainStation,
+    trainEtas: List<TrainEta>,
     googleStreetMapImage: Drawable,
     showGoogleStreetImage: Boolean,
     isFavorite: Boolean
@@ -145,18 +179,7 @@ fun TrainStationView(
                 )
             }
         },
-/*        floatingActionButton = {
-            var clickCount = remember { mutableStateOf(0) }
-            ExtendedFloatingActionButton(
-                text = { Text("Show snackbar") },
-                onClick = {
-                    scope.launch {
-                        snackbarHostState.showSnackbar("Snackbar # ${++clickCount.value}")
-                    }
-                }
-            )
-        },*/
-        content = { innerPadding ->
+        content = {
             val activity = (LocalLifecycleOwner.current as ComponentActivity)
             Column(
                 modifier = modifier
@@ -204,63 +227,118 @@ fun TrainStationView(
                         .zIndex(5f)
                         .fillMaxWidth()
                 ) {
-                    Row(modifier = Modifier.fillMaxWidth()) {
-                        Text(
-                            text = "${trainStation.name} (${trainStation.id})",
-                            style = MaterialTheme.typography.titleLarge
-                        )
-                        IconButton(onClick = { switchFavorite(trainStation.id) }) {
-                            Icon(
-                                imageVector = Icons.Filled.Favorite,
-                                contentDescription = "Favorite",
-                                tint = if (isFavorite) Color(fr.cph.chicago.util.Color.yellowLineDark) else LocalContentColor.current
+                    Column {
+                        Row(modifier = Modifier.fillMaxWidth()) {
+                            Text(
+                                text = "${trainStation.name} (${trainStation.id})",
+                                style = MaterialTheme.typography.titleLarge
                             )
-                        }
-                        val context = LocalContext.current
-                        val (snackbarVisibleState, setSnackBarState) = remember { mutableStateOf(false) }
-                        if (snackbarVisibleState) {
-                            Snackbar(
-                                action = {
-                                    Button(onClick = {}) {
-                                        Text("MyAction")
+                            IconButton(onClick = { switchFavorite(trainStation.id) }) {
+                                Icon(
+                                    imageVector = Icons.Filled.Favorite,
+                                    contentDescription = "Favorite",
+                                    tint = if (isFavorite) Color(fr.cph.chicago.util.Color.yellowLineDark) else LocalContentColor.current
+                                )
+                            }
+                            val context = LocalContext.current
+                            val (snackbarVisibleState, setSnackBarState) = remember { mutableStateOf(false) }
+                            if (snackbarVisibleState) {
+                                Snackbar(
+                                    action = {
+                                        Button(onClick = {}) {
+                                            Text("MyAction")
+                                        }
+                                    },
+                                    modifier = Modifier.padding(8.dp)
+                                ) { Text(text = "This is a snackbar!") }
+                            }
+
+                            IconButton(onClick = {
+                                val uri = String.format(Locale.ENGLISH, "geo:%f,%f", trainStation.stops[0].position.latitude, trainStation.stops[0].position.longitude)
+                                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
+
+                                if (intent.resolveActivity(context.packageManager) != null) {
+                                    context.startActivity(intent)
+                                } else {
+                                    scope.launch {
+                                        snackbarHostState.showSnackbar("Could not find any Map application on device")
                                     }
-                                },
-                                modifier = Modifier.padding(8.dp)
-                            ) { Text(text = "This is a snackbar!") }
+                                }
+                            }) {
+                                Icon(
+                                    imageVector = Icons.Filled.Map,
+                                    contentDescription = "Map",
+                                )
+                            }
                         }
-
-                        IconButton(onClick = {
-                            val uri = String.format(Locale.ENGLISH, "geo:%f,%f", trainStation.stops[0].position.latitude, trainStation.stops[0].position.longitude)
-                            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
-
-                            if (intent.resolveActivity(context.packageManager) != null) {
-                                context.startActivity(intent)
-                            } else {
-                                scope.launch {
-                                    snackbarHostState.showSnackbar("Could not find any Map application on device")
+                        Column(
+                            modifier = Modifier.fillMaxWidth(),
+                        ) {
+                            trainStation.stopByLines.forEach { entry ->
+                                val line = entry.key
+                                val stops = entry.value
+                                Row(
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    horizontalArrangement = Arrangement.Center,
+                                    modifier = Modifier.fillMaxWidth(),
+                                ) {
+                                    Surface(
+                                        color = Color(line.color),
+                                        shadowElevation = 3.dp,
+                                        shape = RoundedCornerShape(15.0.dp),
+                                    ) {
+                                        Text(
+                                            text = line.toStringWithLine(),
+                                            color = Color.White,
+                                            style = MaterialTheme.typography.titleSmall,
+                                            modifier = Modifier.padding(horizontal = 15.dp, vertical = 5.dp),
+                                        )
+                                    }
+                                }
+                                stops.sorted().forEach { stop ->
+                                    Stop(
+                                        stationId = trainStation.id,
+                                        line = line,
+                                        stop = stop,
+                                        trainEtas = trainEtas
+                                            .filter { trainEta -> trainEta.trainStation.id == trainStation.id }
+                                            .filter { trainEta -> trainEta.routeName == line }
+                                    )
                                 }
                             }
-                        }) {
-                            Icon(
-                                imageVector = Icons.Filled.Map,
-                                contentDescription = "Map",
-                            )
-                        }
-                    }
-                    Column {
-                        trainStation.stopByLines.forEach { entry ->
-                            val line = entry.key
-                            val stops = entry.value
-                            Text(
-                                text = line.toStringWithLine()
-                            )
-
                         }
                     }
                 }
             }
         }
     )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Stop(modifier: Modifier = Modifier, stationId: BigInteger, line: TrainLine, stop: Stop, trainEtas: List<TrainEta>) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(Color.Gray),
+    ) {
+        Checkbox(
+            checked = preferenceService.getTrainFilter(stationId, line, stop.direction),
+            onCheckedChange = {},
+            colors = CheckboxDefaults.colors(), // TODO to customize
+        )
+        Text(text = stop.direction.toString())
+        Column {
+            trainEtas
+                .filter { trainEta -> trainEta.stop.direction.toString() == stop.direction.toString() }
+                .forEach { trainEta ->
+                    Row {
+                        Text(text = trainEta.destName + ": ")
+                        Text(text = trainEta.timeLeftDueDelay + " ")
+                    }
+                }
+        }
+    }
 }
 
 fun isFavorite(trainStationId: BigInteger): Boolean {
