7bd04b468901c19fdd6027fa2f49b23cbba8df29
Carl-Philipp Harmant <cp.harmant@gmail.com>
2022-02-17 13:01:50 -0600

    Update popup ui

--- android-app/src/main/kotlin/fr/cph/chicago/core/composable/screen/Favorites.kt
+++ android-app/src/main/kotlin/fr/cph/chicago/core/composable/screen/Favorites.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
@@ -12,12 +13,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DirectionsBike
 import androidx.compose.material.icons.filled.DirectionsBus
 import androidx.compose.material.icons.filled.Train
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledTonalButton
@@ -32,9 +35,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
 import androidx.core.content.ContextCompat.startActivity
@@ -182,15 +187,28 @@ fun BusDetailDialog(show: Boolean, busDetailsDTOs: List<BusDetailsDTO>, hideDial
             onDismissRequest = hideDialog,
             // FIXME workaround because the dialog do not resize after loading. Issue: https://issuetracker.google.com/issues/194911971?pli=1
             properties = DialogProperties(usePlatformDefaultWidth = false),
+            title = {
+                Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+                    Text(
+                        text = "Choose a bus stop",
+                        style = MaterialTheme.typography.titleMedium,
+                    )
+                }
+            },
             text = {
                 Column(
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally,
                     modifier = Modifier.fillMaxWidth(),
                 ) {
-                    busDetailsDTOs.forEach { busDetailsDTO ->
+                    busDetailsDTOs.forEachIndexed() { index, busDetailsDTO ->
+                        val modifier = if (index == busDetailsDTOs.size - 1) {
+                            Modifier.fillMaxWidth()
+                        } else {
+                            Modifier.fillMaxWidth().padding(bottom = 5.dp)
+                        }
                         OutlinedButton(
-                            modifier = Modifier.fillMaxWidth(),
+                            modifier = modifier,
                             onClick = {
                                 startBusDetailActivity(context, busDetailsDTO)
                                 hideDialog()
@@ -199,13 +217,21 @@ fun BusDetailDialog(show: Boolean, busDetailsDTOs: List<BusDetailsDTO>, hideDial
                             Text(
                                 text = "${busDetailsDTO.stopName} (${busDetailsDTO.bound})",
                                 style = MaterialTheme.typography.bodyMedium,
+                                textAlign = TextAlign.Center,
                             )
                         }
                     }
+
                 }
             },
             confirmButton = {},
-            dismissButton = {},
+            dismissButton = {
+                FilledTonalButton(onClick = { hideDialog() }) {
+                    Text(
+                        text = "Dismiss",
+                    )
+                }
+            },
         )
     }
 }
