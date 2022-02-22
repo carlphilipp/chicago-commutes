461ce2f93e84999658f4b26f56b599299dc043d9
Carl-Philipp Harmant <cp.harmant@gmail.com>
2022-02-21 21:22:33 -0600

    Play with themes

--- android-app/src/main/kotlin/fr/cph/chicago/core/composable/screen/Settings.kt
+++ android-app/src/main/kotlin/fr/cph/chicago/core/composable/screen/Settings.kt
@@ -2,38 +2,37 @@ package fr.cph.chicago.core.composable.screen
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.DataExploration
-import androidx.compose.material.icons.filled.DeveloperMode
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.filled.Theaters
 import androidx.compose.material3.Divider
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import fr.cph.chicago.service.PreferenceService
+import timber.log.Timber
 
 @Composable
 fun Settings(modifier: Modifier = Modifier) {
+    val theme = PreferenceService.getTheme()
+    Timber.i("Current theme found: $theme")
     LazyColumn(modifier = modifier.fillMaxWidth()) {
+        val cellModifier = Modifier.padding(15.dp)
         item {
             // Theme
-            Row {
-                Icon(
-                    imageVector = Icons.Filled.Theaters,
-                    contentDescription = "Theme"
-                )
+            Row(modifier = cellModifier) {
                 Column {
                     Text(
                         text = "Theme",
-                        style = MaterialTheme.typography.titleSmall,
+                        style = MaterialTheme.typography.titleMedium,
                     )
+                    Spacer(modifier = Modifier.height(5.dp))
                     Text(
-                        text = "Light",
+                        text = theme.description,
                         style = MaterialTheme.typography.bodyMedium,
                     )
                 }
@@ -42,16 +41,13 @@ fun Settings(modifier: Modifier = Modifier) {
         }
         item {
             // Data cache
-            Row {
-                Icon(
-                    imageVector = Icons.Filled.DataExploration,
-                    contentDescription = "Data cache"
-                )
+            Row(modifier = cellModifier) {
                 Column {
                     Text(
                         text = "Data cache",
-                        style = MaterialTheme.typography.titleSmall,
+                        style = MaterialTheme.typography.titleMedium,
                     )
+                    Spacer(modifier = Modifier.height(5.dp))
                     Text(
                         text = "Clear cache",
                         style = MaterialTheme.typography.bodyMedium,
@@ -66,16 +62,13 @@ fun Settings(modifier: Modifier = Modifier) {
         }
         item {
             // Developer options
-            Row {
-                Icon(
-                    imageVector = Icons.Filled.DeveloperMode,
-                    contentDescription = "Developer mode"
-                )
+            Row(modifier = cellModifier) {
                 Column {
                     Text(
                         text = "Developer options",
-                        style = MaterialTheme.typography.titleSmall,
+                        style = MaterialTheme.typography.titleMedium,
                     )
+                    Spacer(modifier = Modifier.height(5.dp))
                     Text(
                         text = "Show developer options",
                         style = MaterialTheme.typography.bodyMedium,
@@ -87,16 +80,13 @@ fun Settings(modifier: Modifier = Modifier) {
         }
         item {
             // About
-            Row {
-                Icon(
-                    imageVector = Icons.Filled.Settings,
-                    contentDescription = "About"
-                )
+            Row(modifier = cellModifier) {
                 Column {
                     Text(
                         text = "About",
-                        style = MaterialTheme.typography.titleSmall,
+                        style = MaterialTheme.typography.titleMedium,
                     )
+                    Spacer(modifier = Modifier.height(5.dp))
                     Text(
                         text = "Version x.x.x",
                         style = MaterialTheme.typography.bodyMedium,
--- android-app/src/main/kotlin/fr/cph/chicago/core/composable/theme/Theme.kt
+++ android-app/src/main/kotlin/fr/cph/chicago/core/composable/theme/Theme.kt
@@ -3,9 +3,17 @@ package fr.cph.chicago.core.composable.theme
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import fr.cph.chicago.core.model.Theme
+import fr.cph.chicago.service.PreferenceService
 
 @Composable
-fun ChicagoCommutesTheme(isDarkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun ChicagoCommutesTheme(preferenceService: PreferenceService = PreferenceService, content: @Composable () -> Unit) {
+    val isDarkTheme = when (preferenceService.getTheme()) {
+        Theme.AUTO -> isSystemInDarkTheme()
+        Theme.LIGHT -> false
+        Theme.DARK -> true
+    }
+
     val colors = if (isDarkTheme) {
         DarkThemeColors
     } else {
