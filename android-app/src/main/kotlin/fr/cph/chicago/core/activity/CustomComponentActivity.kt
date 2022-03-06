dbb39ff1c6955f1b31c69f3a5419470042f693bd
Carl-Philipp Harmant <cp.harmant@gmail.com>
2022-03-06 16:22:01 -0600

    Add comment

--- android-app/src/main/kotlin/fr/cph/chicago/core/activity/CustomComponentActivity.kt
+++ android-app/src/main/kotlin/fr/cph/chicago/core/activity/CustomComponentActivity.kt
@@ -8,6 +8,10 @@ abstract class CustomComponentActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Turn off the decor fitting system windows (top and bottom)
+        // It means that now we need to handle manually the top padding
+        // This allow to have a somewhat fullscreen
         WindowCompat.setDecorFitsSystemWindows(window, false)
     }
 
