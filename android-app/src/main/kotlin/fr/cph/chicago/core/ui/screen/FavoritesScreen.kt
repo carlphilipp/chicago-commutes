86cb9c5debc97e1e7b8551b9005c5b394b74df05
Carl-Philipp Harmant <cp.harmant@gmail.com>
2022-03-18 21:16:16 -0500

    Change fav details button

--- android-app/src/main/kotlin/fr/cph/chicago/core/ui/screen/FavoritesScreen.kt
+++ android-app/src/main/kotlin/fr/cph/chicago/core/ui/screen/FavoritesScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.material.icons.filled.Train
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -309,7 +308,7 @@ fun FooterCard(modifier: Modifier = Modifier, detailsOnClick: () -> Unit = {}, m
             .padding(top = 2.dp)
             .fillMaxWidth()
     ) {
-        FilledTonalButton(
+        ElevatedButton(
             onClick = { detailsOnClick() },
             shape = defaultButtonShape,
         ) {
