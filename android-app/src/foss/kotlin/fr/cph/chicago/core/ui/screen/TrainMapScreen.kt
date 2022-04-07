118e345f58d2e232a6e4b76a7065e958cbc77e4c
Carl-Philipp Harmant <cp.harmant@gmail.com>
2022-04-07 11:27:47 -0500

    Update a lot of dependencies

--- android-app/src/foss/kotlin/fr/cph/chicago/core/ui/screen/TrainMapScreen.kt
+++ android-app/src/foss/kotlin/fr/cph/chicago/core/ui/screen/TrainMapScreen.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import fr.cph.chicago.core.model.enumeration.TrainLine
 import fr.cph.chicago.core.navigation.NavigationViewModel
+import fr.cph.chicago.core.ui.screen.settings.SettingsViewModel
 import fr.cph.chicago.service.TrainService
 import javax.inject.Inject
 
@@ -13,6 +14,7 @@ import javax.inject.Inject
 fun TrainMapScreen(
     modifier: Modifier = Modifier,
     viewModel: MapTrainViewModel,
+    settingsViewModel: SettingsViewModel,
     navigationViewModel: NavigationViewModel,
     title: String,
 ) {
--- android-app/src/foss/kotlin/fr/cph/chicago/core/viewmodel/FossViewModel.kt
+++ android-app/src/foss/kotlin/fr/cph/chicago/core/viewmodel/FossViewModel.kt
@@ -3,12 +3,14 @@ package fr.cph.chicago.core.viewmodel
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.core.content.ContextCompat
 import timber.log.Timber
 
 val mainViewModel = FossMainViewModel()
 
 class FossMainViewModel() : MainViewModel() {
+    @OptIn(ExperimentalMaterialApi::class)
     override fun startMarket(context: Context) {
         val intent = Intent(Intent.ACTION_VIEW)
         try {
